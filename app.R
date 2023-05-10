3d0fd4a716426689762e2cc02f2e27876812eb82
Lívia Meinhardt <70647606+liviameinhardt@users.noreply.github.com>
2023-05-10 19:59:49 -0300

    adiciona pagina juros

--- app.R
+++ app.R
@@ -14,22 +14,27 @@ ui <- fluidPage(
         h3("Dados de entrada"),
         numericInput("tx_juros", label = "Taxa de juros (%)", value = 10),
         textInput("fluxo_caixa", label = "Fluxo de caixa (separado por vírgulas)",value="-100,-100,100,100,200"),
-        actionButton("calcular", "Calcular"),
+        actionButton("calcular", "Calcular")
     ),
       
     
     mainPanel(
         tabsetPanel(
           
-          tabPanel("Resultados",
-                   h3("Resultados da análise"),
-                   tableOutput("tabela_resultados")
+          tabPanel("Investimentos",
+                   h2("Resultados da Análise"),
+                   tableOutput("tabela_resultados"),
+                   plotOutput("grafico_fluxo_caixa")
           ),
           
-          # tabPanel("Gráfico",
-          #          h3("Gráfico do fluxo de caixa"),
-          #          plotOutput("grafico_fluxo_caixa")
-          # )
+          tabPanel("Taxa de Juros",
+                   h2("Métricas em Funçao da Taxa de Juros"),
+                   tableOutput("tabela_resultados_juros"),
+                   plotOutput("grafico_vpl"),
+                   plotOutput("grafico_roi"),
+                   plotOutput("grafico_payback_simples"),
+                   plotOutput("grafico_payback_descontado")
+          )
         )
     )
    
@@ -51,9 +56,20 @@ server <- function(input, output) {
     tx_juros <- input$tx_juros/100
     
     #trata o output
-    output$tabela_resultados <- renderTable({ gerar_tabela(fluxo_caixa, tx_juros) })
     
-    #output$grafico_fluxo_caixa <- renderPlot({gerar_grafico(fluxo_caixa)})
+    #analise de investimento
+    output$tabela_resultados <- renderTable({ gerar_tabela_investimento(fluxo_caixa, tx_juros) })
+    output$grafico_fluxo_caixa <- renderPlot({grafico_fluxo_caixa(fluxo_caixa)})
+    
+    #analise de taxa de juros
+    tabela_func_juros <-  gerar_tabela_juros(fluxo_caixa)
+    output$tabela_resultados_juros <- renderTable({tabela_func_juros })
+    output$grafico_vpl <- renderPlot({grafico_juros(tabela_func_juros,"vpl",tx_juros*100)})
+    output$grafico_roi <- renderPlot({grafico_juros(tabela_func_juros,"roi",tx_juros*100)})
+    output$grafico_payback_simples <- renderPlot({grafico_juros(tabela_func_juros,"payback_simples",tx_juros*100)})
+    output$grafico_payback_descontado <- renderPlot({grafico_juros(tabela_func_juros,"payback_descontado",tx_juros*100)})
+    
+    # 
     
   })
   
