bce09f1ae7e769904f6b95485147104fdc92a81e
Ademir <a.tomazf@gmail.com>
2023-06-19 12:10:43 -0300

    corrige payback_descontado

--- calculadora.R
+++ calculadora.R
@@ -59,7 +59,6 @@ roi <- function(fluxo_caixa, juros) {
   return(roi*100)
 }
 
-
 #calcula o tempo de payback simples e descontado dado o fluxo de caixa e a taxa de juros
 payback <- function(fluxo_caixa, taxa_juros) {
   
